986de5b6163ca041de01ac26e5cbde79c29b40ed
yOoMarvin <marvin.messenzehl@gmail.com>
2019-11-01 11:31:49 +0100

    router and header

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,13 +1,42 @@
 import React from "react"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from "react-router-dom"
+
+import { Header, Logo, Link, Links, Container, Blue } from "./style"
 
 export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <Header>
+          <Logo>
+            <Blue>matiive</Blue> insurance.
+          </Logo>
+          <Links>
+            <NavLink exact to="/liability">
+              <Link>Private Liability</Link>
+            </NavLink>
+            <NavLink exact to="/">
+              <Link>Home</Link>
+            </NavLink>
+          </Links>
+        </Header>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/liability">
+            <h1>This is matiive</h1>
+          </Route>
+          <Route path="/">
+            <h1>This is matiive</h1>
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   )
 }
--- src/containers/App/style.js
+++ src/containers/App/style.js
@@ -1,1 +1,38 @@
 import styled from "styled-components"
+import { theme } from "../../components/theme"
+
+export const Container = styled.div`
+  padding: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+export const Header = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+export const Logo = styled.h1`
+  font-size: 24px;
+`
+export const Links = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+export const Link = styled.span`
+  color: ${theme.text.tertiary};
+  margin-left: 32px;
+  margin-right: 32px;
+  font-size: 18px;
+  font-weight: 500;
+
+  &:hover {
+    color: ${theme.text.default};
+  }
+`
+export const Blue = styled.span`
+  color: ${theme.brand.default};
+`
