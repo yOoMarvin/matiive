a0c12089c20c6e9df80fabcb235f2cebbfc839fd
yOoMarvin <marvin.messenzehl@gmail.com>
2019-11-01 11:11:24 +0100

    setup

---
+++ src/containers/App/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  )
+}
